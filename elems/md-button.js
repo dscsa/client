cfedf5f25adca0c8e582a14a95bb8368421d940c
akircher <adam.kircher@gmail.com>
2015-10-29 00:02:33 -0700

    Refactor: move complex logic from template to VM

--- elems/md-button.js
+++ elems/md-button.js
@@ -1,11 +1,10 @@
 import {bindable, inject} from 'aurelia-framework';
 
 @bindable('class')
-@bindable('disabled')
-@bindable('primary')
-@bindable('accent')
 @bindable('raised')
 @bindable('fab')
+@bindable('disabled')
+@bindable('color')
 @inject(Element)
 export class MdButtonCustomElement {
 
@@ -13,25 +12,39 @@ export class MdButtonCustomElement {
     this.element = element
   }
 
-  //Not sure why I need this function at all.  Without it click.delegate on md-button
-  //would fire even when button was disabled.  Also just having this function stops
-  //event propogation, so we must redispatch the event
+  //Make sure click.delegate doesn't bubble up when button is disabled.
   click($event) {
-    return  ! this.button || ! this.button.disabled
-    //this.element.dispatchEvent(new MouseEvent($event.type, $event))
+    return ! this.button || ! this.button.disabled
   }
 
-
   attached() {
+    if (this.class)
+      this.button.classList.add(this.class)
 
-    if(this.element.parentElement.tagName != 'MD-DRAWER')
-      return
+    //attributes are made an empty string by default, so we can't just test for truthiness
+    if (this.raised == "")
+      this.button.classList.add('mdl-button--raised')
 
-    this.button.style.width = "100%"
-    this.button.style.height = "auto"
-    this.button.style.padding = "16px 8px"
-    this.button.style['line-height'] = "18px"
-    this.button.style['text-align'] = "left"
+    //primary and accent are dynamic/reactive based on disable so need to be in the template.
+    if (typeof this.color == 'string') {
+      this.color = 'mdl-button--'+(this.color || 'colored')
+    }
+
+    if (this.fab > 0) {
+      this.button.classList.add('mdl-button--fab')
+      this.button.style.height = this.fab+'px'
+      this.button.style.width  = this.fab+'px'
+      this.button.style['font-size'] = this.fab*.66+'px'
+      //this.button.style['min-width'] = this.element.fab+'px'
+    }
+
+    if(this.element.parentElement.tagName == 'MD-DRAWER') {
+      this.button.style.width = "100%"
+      this.button.style.height = "auto"
+      this.button.style.padding = "16px 8px"
+      this.button.style['line-height'] = "18px"
+      this.button.style['text-align'] = "left"
+    }
   }
 
 }
