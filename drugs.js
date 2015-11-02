f96bf2bbf29bc5300a958f4fa2a87ab1e77e571f
akircher <adam.kircher@gmail.com>
2015-11-01 16:16:13 -0800

    Refactor: drugs page and fix numerous bugs

--- drugs.js
+++ drugs.js
@@ -33,28 +33,29 @@ export class drugs {
       brand:'',
       image:'',
       labeler:'',
-      ndc:'',
-      price:''
+      ndc:''
     })
     this.drugs = this.drugs.slice() //Aurelia hack to reactivate the filter
   }
 
+  create() {
+    if ( ! confirm('Are you sure that you want to add this new drug?'))
+      return
+    console.log('adding', this.drug)
+    return this.db.drugs.post(this.drug).then(drug => {
+      this.add()
+      this.drug._id = drug._id
+      this.drug._rev = drug._rev
+    })
+  }
+
   save($event, $this) {
-    //Do not save if clicking around within the same donation.
-    //TODO only save if change occured
-    if ($this.contains($event.relatedTarget))
+    //Do not save if clicking around within the same/new drug.
+    if ( ! this.drug._id || $this.contains($event.relatedTarget))
       return
 
-    if (this.drug._id) {
-      console.log('saving', this.drug)
-      return this.db.drugs.put(this.drug)
-    }
-
-    if (this.drug.name && confirm('Are you sure that you want to add this new drug?')) {
-      console.log('adding', this.drug)
-      this.add()
-      return this.db.drugs.post(this.drug)
-    }
+    console.log('saving', this.drug)
+    return this.db.drugs.put(this.drug)
   }
 }
 
@@ -63,7 +64,7 @@ export class filterValueConverter {
   toView(drugs = [], filter = ''){
     filter = filter.toLowerCase()
     return drugs.filter(drug => {
-      return ~ `${drug.name} ${drug.tracking} ${drug.status}`.toLowerCase().indexOf(filter)
+      return ~ `${drug.name} ${drug.strength} ${drug.form} ${drug.ndc}`.toLowerCase().indexOf(filter)
     })
   }
 }
