0953b4e1987fdffd976301be8c419cba17dbc7c7
akircher <adam.kircher@gmail.com>
2016-02-13 12:21:26 -0800

    Refactor autocomplete to behave more like md-input

--- elems/md-autocomplete.js
+++ elems/md-autocomplete.js
@@ -1,30 +1,13 @@
 import {bindable, bindingMode} from 'aurelia-framework';
 
-@bindable({name:'value', defaultBindingMode:bindingMode.twoWay})
-@bindable('query')
-@bindable('select')
+@bindable({name:'value', defaultBindingMode: bindingMode.twoWay})
 @bindable('disabled')
 @bindable('placeholder')
 export class MdAutocompleteCustomElement {
-
-  bind(parent) {
-    this.parent = parent
-  }
-
-  valueChanged(now, old) {
-    let query = this.query.call(this.parent, now, old)
-
-    if ( ! query.then)
-      query = Promise.resolve(query)
-
-    query.then(items => {
-      this.show = now && items.length
-      this.items = items
-    })
-  }
-
-  action(item) {
-    this.select.call(this.parent, item)
-    this.show = false
+  toggleResults() {
+    //Foucusout removes the results from the view before the click on
+    //the selected result can register.  Delaying a bit prevents this
+    //http://stackoverflow.com/questions/18848738/click-event-not-triggered-after-focusout-event
+    setTimeout(() => this.showResults = ! this.showResults, 100)
   }
 }
