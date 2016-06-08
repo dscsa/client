5585798ad7eedc1fafd5f9bd63729e42797bf697
akircher <adam.kircher@gmail.com>
2016-06-07 18:02:44 -0700

    Fix md-snackbar: ensure upgrade or it might not work

--- elems/md-snackbar.js
+++ elems/md-snackbar.js
@@ -9,6 +9,7 @@ export class MdSnackbarCustomElement {
   }
 
   attached() {
+    componentHandler.upgradeElement(this.element)
     this.element.show = opts => {
       if (typeof opts == 'string')
         opts = {message:opts, timeout:8000}
