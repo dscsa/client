7d34ab057ebe02abdac27aa960182c55d60c4ea4
akircher <adam.kircher@gmail.com>
2016-07-23 18:22:08 -0700

    Increase timeout on autocomplete toggle to make sure transactions can be added

--- src/elems/md-autocomplete.js
+++ src/elems/md-autocomplete.js
@@ -8,6 +8,6 @@ export class MdAutocompleteCustomElement {
     //Foucusout removes the results from the view before the click on
     //the selected result can register.  Delaying a bit prevents this
     //http://stackoverflow.com/questions/18848738/click-event-not-triggered-after-focusout-event
-    setTimeout(() => this.showResults = ! this.showResults, 100)
+    setTimeout(() => this.showResults = ! this.showResults, 200)
   }
 }
