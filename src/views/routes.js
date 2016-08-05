aedca01fde72014f2b373bd51ad437c4ecae5567
akircher <adam.kircher@gmail.com>
2016-08-05 12:34:18 -0700

    Prevent logout on browser tab closing
    It wasn’t actually logging out, but default route was join which forced
    user without knowledge of direct URL to go back to login page which
    logged out user.

--- src/views/routes.js
+++ src/views/routes.js
@@ -6,13 +6,13 @@ export class App {
     config.addPipelineStep('authorize', AuthorizeStep);
 
     config.map([
-      { route: 'login',                        moduleId: 'views/login',     title:'Login',     nav:true},
-      { route: ['join', ''],                   moduleId: 'views/join',      title:'Join',      nav:true},
-      { route: ['inventory', 'inventory/:id'], moduleId: 'views/inventory', title:'Inventory', nav:true, roles:["user"]},
-      { route: ['shipments', 'shipments/:id'], moduleId: 'views/shipments', title:'Shipments', nav:true, roles:["user"]},
-      { route: ['drugs', 'drugs/:id'],         moduleId: 'views/drugs',     title:'Drugs',     nav:true, roles:["user"]},
-      { route: ['records', 'records/:id'],     moduleId: 'views/records',   title:'Records',   nav:true, roles:["user"]},
-      { route: 'account',                      moduleId: 'views/account',   title:'Account',   nav:true, roles:["user"]}
+      { route: 'login',                            moduleId: 'views/login',     title:'Login',     nav:true},
+      { route: ['join'],                           moduleId: 'views/join',      title:'Join',      nav:true},
+      { route: ['inventory', 'inventory/:id'],     moduleId: 'views/inventory', title:'Inventory', nav:true, roles:["user"]},
+      { route: ['shipments', 'shipments/:id', ''], moduleId: 'views/shipments', title:'Shipments', nav:true, roles:["user"]},
+      { route: ['drugs', 'drugs/:id'],             moduleId: 'views/drugs',     title:'Drugs',     nav:true, roles:["user"]},
+      { route: ['records', 'records/:id'],         moduleId: 'views/records',   title:'Records',   nav:true, roles:["user"]},
+      { route: 'account',                          moduleId: 'views/account',   title:'Account',   nav:true, roles:["user"]}
     ]);
 
     this.router = router
