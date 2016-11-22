ef5a1a00c240c48c87e5c65ecfac16224fd2bd7b
akircher <adam.kircher@gmail.com>
2016-11-22 15:10:32 -0800

    Simplify authorization to be inside view model lifecycle

--- src/views/routes.js
+++ src/views/routes.js
@@ -2,9 +2,8 @@
 export class App {
 
   configureRouter(config, router){
-    config.title = 'SIRUM';
-    config.addPipelineStep('authorize', AuthorizeStep);
-
+    this.routes  = router.navigation
+    config.title = 'SIRUM'
     config.map([
       { route: 'login',                            moduleId: 'views/login',     title:'Login',     nav:true},
       { route: ['join'],                           moduleId: 'views/join',      title:'Join',      nav:true},
@@ -14,40 +13,5 @@ export class App {
       { route: ['records', 'records/:id'],         moduleId: 'views/records',   title:'Records',   nav:true, roles:["user"]},
       { route: 'account',                          moduleId: 'views/account',   title:'Account',   nav:true, roles:["user"]}
     ])
-    this.router = router
-  }
-}
-
-import {inject}   from 'aurelia-framework'
-import {Db}       from '../libs/pouch'
-import {Router, Redirect} from 'aurelia-router'
-
-@inject(Router, Db)
-export class AuthorizeStep {
-  constructor(router, db){
-    this.db = db
-    //Unfortunately on browser reload run method is not called
-    //so if session has expired we need to check here as well
-    if ( ! document.cookie)
-      router.navigate('login')
-  }
-
-  run(routing, nextStep) {
-    //Run must return a promise of a navigation route.  i.e., Second parameter is not a pure callback
-    return this.db.user.session.get().then(session => {
-      let next = routing.getAllInstructions()[0].config
-      for (let route of routing.router.navigation) {
-        if ( ! session || ! session.account) {
-          route.isVisible = ! route.config.roles; continue
-        }
-        let role = session.account._id.length == 7 ? 'user' : 'admin'
-        route.isVisible = route.config.roles && ~route.config.roles.indexOf(role)
-      }
-
-      if (next.navModel.isVisible) return nextStep()
-      let redirect = new Redirect(next.navModel.config.roles ? 'login' : 'shipments')
-      return nextStep.cancel(redirect)
-    })
-    .catch(err => console.log('router error', err))
   }
 }
