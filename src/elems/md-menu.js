4efc703774dffef91f6cae1dca7dba8198434449
akircher <adam.kircher@gmail.com>
2016-12-03 15:13:45 -0800

    Enable md-menu to work with disabled property

--- src/elems/md-menu.js
+++ src/elems/md-menu.js
@@ -1,19 +1,38 @@
-import {bindable, inject} from 'aurelia-framework';
+import {ObserverLocator, bindable, inject} from 'aurelia-framework';
 
-@inject(Element)
+@inject(ObserverLocator, Element)
 export class MdMenuCustomElement {
 
-  constructor(element) {
+  constructor(observer, element) {
     //Router add both pages to view at same time.  This confuses the MDL library
     //which add the button's click handler to the previous page rather than the
     //next page since the two ids are the same.  Prevent this by randomly generating
     //the id.  Also 'for.bind' doesn't seem to work so using synomym data-md-for
     this.id = 'id'+Date.now()
     this.element = element
+    this.observer = observer
+  }
+
+  click($event) {
+    if ($event.target.tagName == 'INPUT' || $event.target.disabled)
+      $event.stopImmediatePropagation()
+
+    return true //needed to continue a propogation of an <a> link
+  }
+
+  setDisabled(li, disabled) {
+    disabled
+      ? li.setAttribute('disabled', '')
+      : li.removeAttribute('disabled')
   }
 
   attached() {
-    for (let li of this.element.querySelectorAll('li'))
+    for (let li of this.element.querySelectorAll('li')) {
       li.classList.add('mdl-menu__item')
+      this.setDisabled(li, li.disabled)
+      this.observer.getObserver(li, 'disabled').subscribe(disabled => {
+        this.setDisabled(li, disabled)
+      })
+    }
   }
 }
